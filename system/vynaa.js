78e969784bfa301d6013b8fc290090aa79b3b8c2
Vynaa <118973846+VynaaValerie@users.noreply.github.com>
2025-05-21 15:25:11 +0700

    Add files via upload

---
+++ system/vynaa.js
@@ -0,0 +1,49 @@
+// System Configuration File
+export const info = {
+    // Basic Bot Information
+    namebot: "VynaaBot-MD",
+    wm: "VynaaBot-MD | © 2024",
+    
+    // Owner Information
+    nomerown: "6281234567890",
+    ownername: "Vynaa Valerie",
+    ownerbio: "Bot Developer & Freelancer",
+    owneremail: "vynaavalerie@vlshop.me",
+    ownerlocation: "🇮🇩 Indonesia, Pekanbaru",
+    ownerwebsite: "https://linkbio.co/VLShop",
+    ownerrole: "Creator Vynaa AI",
+    owneryoutube: "https://youtube.com/@VynaaChan",
+    ownerinstagram: "https://instagram.com/vynaa_valerie",
+    ownerdiscord: "https://discord.gg/c6wYDZfhhc",
+    ownerwhatsappchannel: "https://whatsapp.com/channel/0029VaHPYh6LNSa81M9Xcq1K",
+    
+    ownerdescription: "A bot creator and freelance developer...",
+    ownerservices: "- Custom Bot Development\n- Business Automation...",
+    
+    // Bot Information
+    botname: "VynaaBot-MD",
+    botbio: "Multidevice WhatsApp Bot",
+    botemail: "bot@example.com",
+    botlocation: "🇮🇩 Indonesia",
+    botwebsite: "https://example.com",
+    botrole: "Multipurpose WhatsApp Bot"
+}
+
+global.info = info
+
+
+/*
+    ••JANGAN HAPUS INI••
+SCRIPT BY © VYNAA VALERIE 
+•• recode kasih credits 
+•• contacts: (6282389924037)
+•• instagram: @vynaa_valerie 
+•• (github.com/VynaaValerie) 
+
+
+Aturan:
+1. Dilarang memperjualbelikan script ini.
+2. Hak cipta milik Vynaa Valerie.
+
+“Dan janganlah kamu makan harta di antara kamu dengan jalan yang batil, dan janganlah kamu membunuh dirimu sendiri. Sesungguhnya Allah adalah Maha Penyayang kepadamu.” (QS. Al-Baqarah: 188)
+*/
